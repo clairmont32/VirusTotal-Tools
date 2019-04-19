93c959479d44dde66e7279b6a89a76133286c629
Matthew Clairmont <matthew.clairmont1@gmail.com>
2019-04-19 13:06:21 +0000

    Add to getDomainReport

--- go/src/vtDomainReport.go
+++ go/src/vtDomainReport.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"VirusTotal-Tools/go/src/vtapi"
+	"bufio"
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,21 +11,35 @@ import (
 	"time"
 )
 
-func getDomainReport(d string) {
+func getDomainReport(d string) (buffer bytes.Buffer) {
 	client := http.Client{Timeout: time.Duration(10) * time.Second}
 	reportUrl := "http://www.virustotal.com/vtapi/v2/url/report"
 
-	req, err := client.PostForm(reportUrl, url.Values{"apikey": {apikey.Key}, "resource": {"rockstargames.com"}})
+	resp, err := client.PostForm(reportUrl, url.Values{"apikey": {apikey.Key}, "resource": {"rockstargames.com"}})
 	if err != nil {
-		log.Fatalf("Improper request given\n %v", err)
+		log.Fatalf("Improper requeest given\n %v", err)
 	}
-	if req.StatusCode == http.StatusNoContent {
+	if resp.StatusCode == http.StatusNoContent {
 		fmt.Println("Rate-limit exceeded. Trying again in 15 seconds...")
 		time.Sleep(time.Duration(15) * time.Second)
 		getDomainReport(d)
 	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		// TODO: sanitize domain for printing/logging
+		fmt.Printf("No results found for %v", d)
+	}
+
+	if resp.StatusCode == http.StatusOK {
+		if _, bufErr := buffer.ReadFrom(resp.Body); bufErr != nil {
+			log.Fatalf("Failed to read response into buffer\n%v", bufErr)
+		}
+		return buffer
+	}
+	return
 }
 
 func main() {
+ // TODO: verify above works. Write a test for VT response codes?
 
 }
